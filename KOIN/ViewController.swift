b55c77decde04246fc31604cc188aa8a8a43a0a5
Richardd <richardrikimaru@gmail.com>
2017-12-01 13:40:22 +0700

    Initial Commit

---
+++ KOIN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KOIN
+//
+//  Created by Richard Richard on 01/12/17.
+//  Copyright © 2017 Richard Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
